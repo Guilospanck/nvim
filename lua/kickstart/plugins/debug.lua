ac93408412319b4b88566c562ea530c69d4b7f76
Guilherme Pereira <guilospanck@protonmail.com>
2024-06-16 17:56:46 +0100

    Fix rust debuggger

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -1,5 +1,4 @@
 -- debug.lua
---
 -- Shows how to use the DAP plugin to debug your code.
 --
 return {
@@ -17,8 +16,28 @@ return {
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
 
-    -- Add your own debuggers here
+    -- go
     'leoluz/nvim-dap-go',
+    -- rust
+    {
+      'mrcjkb/rustaceanvim',
+      version = '^4',
+      lazy = false,
+      config = function()
+        vim.g.rustaceanvim = {
+          server = {
+            cmd = function()
+              local mason_registry = require 'mason-registry'
+              local ra_binary = mason_registry.is_installed 'rust-analyzer'
+                  -- This may need to be tweaked, depending on the operating system.
+                  and mason_registry.get_package('rust-analyzer'):get_install_path() .. '/rust-analyzer'
+                or 'rust-analyzer'
+              return { ra_binary } -- You can add args to the list, such as '--log-file'
+            end,
+          },
+        }
+      end,
+    },
   },
   config = function()
     local dap = require 'dap'
@@ -36,6 +55,7 @@ return {
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'rust=analyzer',
       },
     }
 
@@ -85,139 +105,9 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     ---------------------------------------------------------------------------
-    -- Config rust debugger
+    -- Config go
     ---------------------------------------------------------------------------
-    -- dap.adapters.lldb = {
-    --   type = 'executable',
-    --   command = '/opt/homebrew/opt/llvm/bin/lldb-dap',
-    --   name = 'lldb',
-    -- }
-    -- local common_launch_configs = {
-    --   name = 'Launch',
-    --   type = 'lldb',
-    --   request = 'launch',
-    --   program = function()
-    --     return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-    --   end,
-    --   cwd = '${workspaceFolder}',
-    --   stopOnEntry = false,
-    --   args = {},
-    --   env = function()
-    --     local variables = {}
-    --     for k, v in pairs(vim.fn.environ()) do
-    --       table.insert(variables, string.format('%s=%s', k, v))
-    --     end
-    --     return variables
-    --   end,
-    -- }
-    -- local common_attach_configs = {
-    --   -- If you get an "Operation not permitted" error using this, try disabling YAMA:
-    --   --  echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-    --   name = 'Attach to process',
-    --   type = 'cpp', -- Adjust this to match your adapter name (`dap.adapters.<name>`)
-    --   request = 'attach',
-    --   pid = require('dap.utils').pick_process,
-    --   args = {},
-    --   env = function()
-    --     local variables = {}
-    --     for k, v in pairs(vim.fn.environ()) do
-    --       table.insert(variables, string.format('%s=%s', k, v))
-    --     end
-    --     return variables
-    --   end,
-    -- }
-    -- dap.configurations.cpp = {
-    --   common_launch_configs,
-    --   common_attach_configs,
-    -- }
-    -- dap.configurations.c = dap.configurations.cpp
     --
-    -- local rust_launch_configs = common_launch_configs
-    -- local rust_attach_configs = common_attach_configs
-    -- local initCommands = function()
-    --   -- Find out where to look for the pretty printer Python module
-    --   local rustc_sysroot = vim.fn.trim(vim.fn.system 'rustc --print sysroot')
-    --
-    --   local script_import = 'command script import "' .. rustc_sysroot .. '/lib/rustlib/etc/lldb_lookup.py"'
-    --   local commands_file = rustc_sysroot .. '/lib/rustlib/etc/lldb_commands'
-    --
-    --   local commands = {}
-    --   local file = io.open(commands_file, 'r')
-    --   if file then
-    --     for line in file:lines() do
-    --       table.insert(commands, line)
-    --     end
-    --     file:close()
-    --   end
-    --   table.insert(commands, 1, script_import)
-    --
-    --   return commands
-    -- end
-    -- rust_launch_configs['initCommands'] = initCommands
-    -- rust_attach_configs['initCommands'] = initCommands
-    --
-    -- dap.configurations.rust = {
-    --   rust_launch_configs,
-    --   rust_attach_configs,
-    -- }
-    local ExecTypes = {
-      TEST = 'cargo build --tests -q --message-format=json',
-      BIN = 'cargo build -q --message-format=json',
-    }
-
-    local function runBuild(type)
-      local lines = vim.fn.systemlist(type)
-      local output = table.concat(lines, '\n')
-      local filename = output:match '^.*"executable":"(.*)",.*\n.*,"success":true}$'
-
-      if filename == nil then
-        return error 'failed to build cargo project'
-      end
-
-      return filename
-    end
-
-    dap.adapters.codelldb = {
-      type = 'server',
-      port = '${port}',
-      executable = {
-        command = vim.fn.exepath 'codelldb', -- install `lldb` && use :Mason to install codelldb & cpptools
-        args = { '--port', '${port}' },
-        detached = true,
-      },
-      name = 'codelldb',
-    }
-
-    if vim.fn.has 'win32' == 1 then
-      dap.adapters.codelldb.executable.detached = false
-    end
-
-    dap.configurations.rust = {
-      {
-        name = 'Debug Test',
-        type = 'codelldb',
-        request = 'launch',
-        program = function()
-          return runBuild(ExecTypes.TEST)
-        end,
-        cwd = '${workspaceFolder}',
-        stopOnEntry = false,
-        showDisassembly = 'never',
-      },
-      {
-        name = 'Debug Bin',
-        type = 'codelldb',
-        request = 'launch',
-        program = function()
-          return runBuild(ExecTypes.BIN)
-        end,
-        cwd = '${workspaceFolder}',
-        stopOnEntry = false,
-        showDisassembly = 'never',
-      },
-    }
-
-    -- Install golang specific config
     require('dap-go').setup {
       delve = {
         -- On Windows delve must be run attached or it crashes.
