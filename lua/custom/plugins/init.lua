c282d3466233546bece68bb2053b8ec242239179
Guilherme Pereira <guilospanck@protonmail.com>
2024-06-18 05:17:00 +0100

    Fix harpoon

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -16,53 +16,31 @@ return {
   {
     'ThePrimeagen/harpoon',
     branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+    dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local harpoon = require 'harpoon'
-
-      -- REQUIRED
       harpoon:setup()
-      -- REQUIRED
-      --
+      local list = harpoon:list()
+
       -- CMD SHIFT M
       vim.keymap.set('n', '', function()
-        harpoon:list():add()
+        local abs_path = vim.fn.expand '%:p'
+        list:add { value = abs_path }
       end, { desc = 'Add to harpoon window' })
 
-      -- WARN: currently the telescope is buggy. Without it works fine
-      -- -- basic telescope configuration
-      -- local conf = require('telescope.config').values
-      -- local function toggle_telescope(harpoon_files)
-      --   local file_paths = {}
-      --   for _, item in ipairs(harpoon_files.items) do
-      --     table.insert(file_paths, item.value)
-      --   end
-      --
-      --   require('telescope.pickers')
-      --     .new({}, {
-      --       prompt_title = 'Harpoon',
-      --       finder = require('telescope.finders').new_table {
-      --         results = file_paths,
-      --       },
-      --       previewer = conf.file_previewer {},
-      --       sorter = conf.generic_sorter {},
-      --     })
-      --     :find()
-      -- end
-      --
       -- CMD SHIFT E
       vim.keymap.set('n', '', function()
-        harpoon.ui:toggle_quick_menu(harpoon:list())
+        harpoon.ui:toggle_quick_menu(list)
       end, { desc = 'Open harpoon window' })
 
       -- Toggle previous & next buffers stored within Harpoon list
       -- CMD SHIFT P
       vim.keymap.set('n', '', function()
-        harpoon:list():prev()
+        list:prev()
       end, { desc = 'Previous harpoon item' })
       -- CMD SHIFT N
       vim.keymap.set('n', '', function()
-        harpoon:list():next()
+        list:next()
       end, { desc = 'Next harpoon item' })
     end,
   },
