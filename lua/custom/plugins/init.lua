b8dae4cec586697284303eca5a9ee29015a40e0b
Guilherme Pereira <guilospanck@protonmail.com>
2024-06-21 13:56:38 +0100

    Add options to undotree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -47,6 +47,10 @@ return {
   {
     'mbbill/undotree',
     config = function()
+      vim.g.undotree_SetFocusWhenToggle = 1
+      vim.g.undotree_DiffpanelHeight = 20
+      vim.g.undotree_SplitWidth = 80
+      vim.g.undotree_WindowLayout = 2
       vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
     end,
   },
