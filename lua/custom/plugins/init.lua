1828827bae5c0b0a84c8aeef1ca6f54f7816fc0f
Guilherme Pereira <guilospanck@protonmail.com>
2024-06-29 19:45:28 +0100

    Change undo panel width

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -71,7 +71,7 @@ return {
     config = function()
       vim.g.undotree_SetFocusWhenToggle = 1
       vim.g.undotree_DiffpanelHeight = 20
-      vim.g.undotree_SplitWidth = 80
+      vim.g.undotree_SplitWidth = 30
       vim.g.undotree_WindowLayout = 2
       vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
     end,
